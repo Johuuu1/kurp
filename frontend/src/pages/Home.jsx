30d1947694e199e9b889edd1357c95c1b9fa7d7e
norlit <example123@inbox.com>
2023-04-18 11:41:43 +0300

    Initial commit

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Grid from '@mui/material/Grid';
+
+import { Post } from '../components/Post';
+import { TagsBlock } from '../components/TagsBlock';
+import { CommentsBlock } from '../components/CommentsBlock';
+import { fetchPosts, fetchTags } from '../redux/slices/posts';
+
+export const Home = () => {
+  const dispatch = useDispatch();
+  const userData = useSelector((state) => state.auth.data);
+  const { posts, tags } = useSelector((state) => state.posts);
+
+  const isPostsLoading = posts.status === 'loading';
+  const isTagsLoading = tags.status === 'loading';
+
+  React.useEffect(() => {
+    dispatch(fetchPosts());
+    dispatch(fetchTags());
+  }, []);
+
+  return (
+    <>
+      <Tabs style={{ marginBottom: 15 }} value={0} aria-label="basic tabs example">
+        <Tab label="Новые посты" />
+      </Tabs>
+      <Grid container spacing={4}>
+        <Grid xs={8} item>
+          {(isPostsLoading ? [...Array(5)] : posts.items).map((obj, index) =>
+            isPostsLoading ? (
+              <Post key={index} isLoading={true} />
+            ) : (
+              <Post
+                id={obj._id}
+                title={obj.title}
+                imageUrl={obj.imageUrl ? `http://localhost:4444${obj.imageUrl}` : ''}
+                user={obj.user}
+                createdAt={obj.createdAt}
+                viewsCount={obj.viewsCount}
+                commentsCount={0}
+                tags={obj.tags}
+                isEditable={userData?._id === obj.user._id}
+              />
+            ),
+          )}
+        </Grid>
+        <Grid xs={4} item>
+          <TagsBlock items={tags.items} isLoading={isTagsLoading} />
+          <CommentsBlock
+            items={[
+              {
+                user: {
+                  fullName: 'Admin',
+                  avatarUrl: 'adminreveal.png',
+                },
+                text: "It's alive!",
+              },
+              {
+                user: {
+                  fullName: 'Любитель смешных картинок №3521',
+                  avatarUrl: 'girfishing.png',
+                },
+                text: "Я обожаю этот сайт",
+              },
+              {
+                user: {
+                  fullName: 'John Doe',
+                  avatarUrl: 'noavatar.png',
+                },
+                text: 'Its literally the worst site Ive seen in my entire life',
+              },
+            ]}
+            isLoading={false}
+          />
+        </Grid>
+      </Grid>
+    </>
+  );
+};
